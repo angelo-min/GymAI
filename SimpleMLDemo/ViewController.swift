b30a89d64c83e711417e3a21d666966e2d10546b
Gabriele Fioretti <fiorettigabriele@gmail.com>
2020-04-01 20:58:53 +0200

    Initial Commit

---
+++ SimpleMLDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleMLDemo
+//
+//  Created by Gabriele Fioretti on 01/04/2020.
+//  Copyright © 2020 Gabriele Fioretti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
